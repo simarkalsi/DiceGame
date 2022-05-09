007e983070734bb73c58fd087c41d918db768b07
Simarjeet kalsi <singhsimar280@gmail.com>
2022-05-09 23:55:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+
+var randomNumber1 = Math.floor(Math.random() * 7);
+var randomNumber2 = Math.floor(Math.random() * 7);
+
+if (randomNumber1 === 1) {
+    document.querySelector(".img1").setAttribute("src", "Images/dice1.png");
+}
+else if (randomNumber1 === 2) {
+    document.querySelector(".img1").setAttribute("src", "Images/dice2.png");
+}
+else if (randomNumber1 === 3) {
+    document.querySelector(".img1").setAttribute("src", "Images/dice3.png");
+}
+else if (randomNumber1 === 4) {
+    document.querySelector(".img1").setAttribute("src", "Images/dice4.png");
+}
+else if (randomNumber1 === 5) {
+    document.querySelector(".img1").setAttribute("src", "Images/dice5.png");
+}
+else {
+    document.querySelector(".img1").setAttribute("src", "Images/dice6.png");
+}
+
+switch (randomNumber2) {
+    case 1:
+        document.querySelector(".img2").setAttribute("src", "Images/dice1.png");
+        break;
+    case 2:
+        document.querySelector(".img2").setAttribute("src", "Images/dice2.png");
+        break;
+    case 3:
+        document.querySelector(".img2").setAttribute("src", "Images/dice3.png");
+        break;
+    case 4:
+        document.querySelector(".img2").setAttribute("src", "Images/dice4.png");
+        break;
+    case 5:
+        document.querySelector(".img2").setAttribute("src", "Images/dice5.png");
+        break;
+    case 6:
+        document.querySelector(".img2").setAttribute("src", "Images/dice6.png");
+        break;
+
+} 
