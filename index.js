0f295331f107ea30711082f65392674d342168c2
Simarjeet kalsi <singhsimar280@gmail.com>
2022-05-10 00:49:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-var randomNumber1 = Math.floor(Math.random() * 7);
-var randomNumber2 = Math.floor(Math.random() * 7);
+var randomNumber1 = Math.floor((Math.random() * 6)+1);
+var randomNumber2 = Math.floor((Math.random() * 6)+1);
 
 if (randomNumber1 === 1) {
     document.querySelector(".img1").setAttribute("src", "Images/dice1.png");
